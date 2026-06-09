52d6a2dd4a6ec9eef5450a3d9dc07745f3249eff
sagarlama0102 <sagarlama0102@gmail.com>
2026-06-09 18:44:26 +0545

    made plan dto

---
+++ src/dtos/plan.dto.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+import { PlanSchema } from "../types/plan.type";
+
+export const CreatePlanDTO = PlanSchema.pick(
+    {
+        title: true,
+        description: true,
+        category: true,
+        coverImage: true,
+        location: true,
+        time: true,
+        date: true,
+        isPublic: true,
+        maxMembers: true
+    }
+)
+export type CreatePlanDTO = z.infer<typeof CreatePlanDTO>;
+
+export const UpdatePlanDTO = CreatePlanDTO.partial();
+
+export type UpdatePlanDTO = z.infer<typeof UpdatePlanDTO>;
